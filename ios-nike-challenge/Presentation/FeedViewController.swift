32a7f707b3f8166bd91adc262c367b8351d846d3
Matias Glessi <matiasglessi@gmail.com>
2021-01-30 08:29:42 -0300

    Add simple tableView

--- ios-nike-challenge/Presentation/FeedViewController.swift
+++ ios-nike-challenge/Presentation/FeedViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class FeedViewController: UIViewController {
     
     private let viewModel: FeedViewModel
+    private let tableView = UITableView()
     
     init(viewModel: FeedViewModel) {
         self.viewModel = viewModel
@@ -20,5 +21,15 @@ class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
 }
